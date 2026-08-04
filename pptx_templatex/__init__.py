628f786e7f2a5d67ab4840393faf4f7fcb896289
Spring_MT <makoto.haruyama@knowledgework.com>
2026-08-04 15:11:15 +0900

    bump version

--- pptx_templatex/__init__.py
+++ pptx_templatex/__init__.py
@@ -3,5 +3,5 @@
 from .exceptions import PlaceholderError, TemplateError
 from .template_engine import TemplateEngine
 
-__version__ = "0.1.3"
+__version__ = "0.2.0"
 __all__ = ["TemplateEngine", "TemplateError", "PlaceholderError"]
